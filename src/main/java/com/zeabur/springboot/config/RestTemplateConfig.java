1dd7ba3d3db046c6035102f6c4dac8f77f2fd8c6
ESDLIFE\joet2 <joetsang@esdlife.com>
2024-05-31 12:41:19 +0800

    udpated rest template config and added interceptor logger

--- src/main/java/com/zeabur/springboot/config/RestTemplateConfig.java
+++ src/main/java/com/zeabur/springboot/config/RestTemplateConfig.java
@@ -1,39 +1,81 @@
 package com.zeabur.springboot.config;
 
-import org.apache.hc.client5.http.config.RequestConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
 @Configuration
+@Slf4j
 public class RestTemplateConfig {
     /**
-     * Creates a standard RestTemplate bean with default HTTP settings.
+     * Helper method to create a RestTemplate that trusts all SSL certificates.
      *
-     * @return a configured RestTemplate instance.
+     * @return a RestTemplate instance configured to trust all SSL certificates.
+     * @throws GeneralSecurityException if there is a problem with the SSL context configuration.
      */
     @Bean
-    public RestTemplate restTemplate() {
-        // Configure HTTP client with redirects enabled.
-        RequestConfig config = RequestConfig.custom()
-                .setRedirectsEnabled(true) // Enable redirects
+    public RestTemplate createRestTemplateWithTrustAllCerts() throws GeneralSecurityException {
+        // Create a trust strategy that accepts all certificates.
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] cert, String authType) -> true;
+
+        // Build an SSL context using the trust strategy.
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
                 .build();
 
-        // Create a CloseableHttpClient with the above configuration.
+        // Create an SSL connection socket factory with the SSL context and no hostname verifier.
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+
+        // Create a registry of connection socket factories for HTTP and HTTPS.
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", sslsf)
+                .register("http", new PlainConnectionSocketFactory())
+                .build();
+
+        // Create a connection manager using the socket factory registry.
+        BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+
+        // Create an HttpClient with the connection manager.
         CloseableHttpClient httpClient = HttpClients.custom()
-                .setDefaultRequestConfig(config)
+                .setConnectionManager(connectionManager)
+                .addRequestInterceptorFirst((request, entity, context) -> {
+                    if (context.getAttribute("http.cookie-origin") != null) {
+                        log.info("CCGames API Request: {} {}", request.getMethod(), request.getPath());
+                    }else {
+                        log.info("CCGames API Direct Request: {} {}", request.getMethod(), request.getPath());
+                    }
+                })
+                .addResponseInterceptorLast((response, entity, context) -> {
+                    if (response.getCode() != 301) {
+                        log.info("CCGames API Response: {} {}", response.getCode(), response.getReasonPhrase());
+                    }else {
+                        log.info("CCGames API Redirect Response: {} {}", response.getCode(), response.getReasonPhrase());
+                    }
+                })
                 .build();
 
-        // Create a factory to use the HttpClient with the RestTemplate.
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        // Create a request factory to use the HttpClient with the RestTemplate.
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
-        // Return a new RestTemplate instance using the factory.
-        return new RestTemplate(factory);
+        // Return a new RestTemplate instance using the request factory.
+        return new RestTemplate(requestFactory);
     }
-
-
 }
 
--- src/main/java/com/zeabur/springboot/helper/RestHelperImpl.java
+++ src/main/java/com/zeabur/springboot/helper/RestHelperImpl.java
@@ -1,40 +1,24 @@
 package com.zeabur.springboot.helper;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
-import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.http.config.Registry;
-import org.apache.hc.core5.http.config.RegistryBuilder;
-import org.apache.hc.core5.ssl.SSLContexts;
-import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.GeneralSecurityException;
 import java.util.Map;
 
 @Slf4j
 @Service
 public class RestHelperImpl implements RestHelper {
     private final RestTemplate restTemplate;
-    private RestTemplate restTemplateIgnoreSSL;
+    private final RestTemplate restTemplateWithTrustAllCerts;
 
-    public RestHelperImpl() {
+    public RestHelperImpl(RestTemplate restTemplateWithTrustAllCerts) {
         this.restTemplate = new RestTemplate();
+        this.restTemplateWithTrustAllCerts = restTemplateWithTrustAllCerts;
     }
 
     @Override
@@ -60,55 +44,17 @@ public class RestHelperImpl implements RestHelper {
             return null;
         }
         // Create and populate HttpHeaders
-        HttpHeaders httpHeaders = new HttpHeaders();
-        if (headersMap != null) {
-            headersMap.forEach(httpHeaders::set);
-        }
-
-        // Prepare an HttpEntity; you might not need a body so just pass the headers
-        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
         try {
-            RestTemplate client = ignoreSSL ? getRestTemplateIgnoreSSL() : restTemplate;
-            // Log the API call
-            log.info("Calling 3rd party API: {}", new URI(url).getPath());
-            ResponseEntity<T> responseEntity = client.exchange(url, HttpMethod.GET, entity, responseClass);
-            // Log the API call success
-            log.info("3rd party API called, response status code: {}", responseEntity.getStatusCode());
-            return responseEntity;
+            HttpHeaders httpHeaders = new HttpHeaders();
+            if (headersMap != null) {
+                headersMap.forEach(httpHeaders::set);
+            }
+            RestTemplate client = ignoreSSL ? restTemplateWithTrustAllCerts : restTemplate;
+            return client.exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders), responseClass);
         } catch (Exception e) {
             logException(e);
         }
         return null;
-    }
-
-    private synchronized RestTemplate getRestTemplateIgnoreSSL() {
-        try {
-            restTemplateIgnoreSSL = createRestTemplateWithTrustAllCerts();
-        } catch (GeneralSecurityException e) {
-            logException(e);
-        }
-        return restTemplateIgnoreSSL;
-    }
-
-    private RestTemplate createRestTemplateWithTrustAllCerts() throws GeneralSecurityException {
-        TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
-        SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-
-        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("https", sslsf)
-                .register("http", new PlainConnectionSocketFactory())
-                .build();
-
-        HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(connectionManager)
-                .build();
-
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        return new RestTemplate(requestFactory);
     }
 
     private void logException(Exception e) {
